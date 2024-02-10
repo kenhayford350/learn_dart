83a87c7202a05988f9ed2b34a84c166b65939242
kenhayford350 <hkenneth350@gmail.com>
2024-02-10 18:04:15 +0000

    first changes

---
+++ 2_variables.dart
@@ -0,0 +1,9 @@
+int main() {
+    var name = "Ken Hayford";
+    var age = 24;
+    var district = "Kasese";
+
+    print("My name is ${name}, I am ${age}, years old, i come from ${district}.");
+
+    return 0;  
+}
---
+++ 3_operators.dart
@@ -0,0 +1,26 @@
+int main() {
+    /*
+   int x = 2;
+   int y = 3;
+   int z = 0;
+
+   z = x - y;
+
+   print("$x - $y = $z");*/
+
+   int a = 2;
+   //a++;
+   //a--;
+
+   //a = a + 3;
+   //a += 3;
+   //a -= 3;
+
+   int m m = 101;
+   int n = m % 3;
+
+   print(n);
+
+   print(a);
+   return 0;
+}
